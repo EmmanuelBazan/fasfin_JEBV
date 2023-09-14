d04a8f5d367e7083c30bbffcf0095f6d0589bcf6
EmmanuelBazan <emmanuelbazan99@gmail.com>
2023-09-14 13:12:51 -0600

    screens implementation

---
+++ lib/app/presentation/pages/record/views/add_record_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AddRecordView extends StatelessWidget {
+  const AddRecordView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('AddRecordView'),
+      ),
+    );
+  }
+}
